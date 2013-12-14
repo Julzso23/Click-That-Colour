308f7849e38e01cebd3d45f7ef837bf5f07dd206
Julzso23 <julzso23@gmail.com>
2013-12-14 12:26:00 -0800

    Initial commit.

---
+++ conf.lua
@@ -0,0 +1,33 @@
+function love.conf(t)
+    t.identity = "CTC"
+    t.version = "0.9.0"
+    t.console = false
+
+    t.window.title = "Click That Colour"
+    t.window.icon = nil
+    t.window.width = 1280
+    t.window.height = 720
+    t.window.borderless = false
+    t.window.resizable = false
+    t.window.minwidth = 1
+    t.window.minheight = 1
+    t.window.fullscreen = false
+    t.window.fullscreentype = "normal"
+    t.window.vsync = true
+    t.window.fsaa = 0
+    t.window.display = 1
+
+    t.modules.audio = true
+    t.modules.event = true
+    t.modules.graphics = true
+    t.modules.image = true
+    t.modules.joystick = true
+    t.modules.keyboard = true
+    t.modules.math = true
+    t.modules.mouse = true
+    t.modules.physics = true
+    t.modules.sound = true
+    t.modules.system = true
+    t.modules.timer = true
+    t.modules.window = true
+end
